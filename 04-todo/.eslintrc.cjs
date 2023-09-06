7b8bc24ea3f9a862e9b1806448c1c844b7290033
César Guerra <warderer@gmail.com>
2023-09-06 14:35:37 -0600

    🚨 Config de Linter con Standard JS

--- 04-todo/.eslintrc.cjs
+++ 04-todo/.eslintrc.cjs
@@ -6,15 +6,17 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'standard',
+    'standard-jsx'
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
-    ],
-  },
+      { allowConstantExport: true }
+    ]
+  }
 }
