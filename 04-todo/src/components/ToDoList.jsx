893006153e37bc8912330079e5cc1089c654c8a8
César Guerra <warderer@gmail.com>
2023-09-06 15:27:31 -0600

    ✨ ToDoList Basico

--- 04-todo/src/components/ToDoList.jsx
+++ 04-todo/src/components/ToDoList.jsx
@@ -5,11 +5,19 @@ const ToDoList = () => {
   // Guardo lo que se escriba en el input
   const [inputValue, setInputValue] = useState('')
 
+  const handleAdd = () => {
+    console.log('Agregue Tarea', inputValue)
+  }
+
   return (
     <div>
       <h1>Lista de Tareas</h1>
-      <input type='text' />
-      <button>Agregar</button>
+      <input
+        type='text'
+        value={inputValue}
+        onChange={(event) => { setInputValue(event.target.value) }}
+      />
+      <button onClick={handleAdd}>Agregar</button>
       <ul>
         <li>Item 1<button>eliminar</button></li>
         <li>Item 2<button>eliminar</button></li>
