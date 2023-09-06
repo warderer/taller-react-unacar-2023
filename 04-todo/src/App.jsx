fae23b69e7cb7d122eb021a87fd11136d7c79685
César Guerra <warderer@gmail.com>
2023-09-06 15:18:31 -0600

    ✨ Estructura ToDoList

--- 04-todo/src/App.jsx
+++ 04-todo/src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
+import ToDoList from './components/ToDoList'
 
 function App () {
   return (
     <>
-
+      <ToDoList />
     </>
   )
 }
---
+++ 04-todo/src/components/ToDoList.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+// Los estados en React sirven para guardar información que se va a utilizar en el componente. Esta información tiene la particularidad de que se  actualiza la vista automáticamente cuando cambia el estado.
+const ToDoList = () => {
+  // Guardo lo que se escriba en el input
+  const [inputValue, setInputValue] = useState('')
+
+  return (
+    <div>
+      <h1>Lista de Tareas</h1>
+      <input type='text' />
+      <button>Agregar</button>
+      <ul>
+        <li>Item 1<button>eliminar</button></li>
+        <li>Item 2<button>eliminar</button></li>
+        <li>Item 3<button>eliminar</button></li>
+      </ul>
+    </div>
+  )
+}
+export default ToDoList
