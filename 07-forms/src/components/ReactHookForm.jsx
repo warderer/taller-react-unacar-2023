e6d3b2ce77fc4fe994d44585ac48a2081d52c11c
César Guerra <warderer@gmail.com>
2023-09-12 15:03:19 -0600

    ✨ ReactHookForm con validaciones HTML

--- 07-forms/src/components/ReactHookForm.jsx
+++ 07-forms/src/components/ReactHookForm.jsx
@@ -1,15 +1,21 @@
+import { useForm } from 'react-hook-form'
 import logo from '../assets/react.svg'
 
 const ReactHookForm = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm()
+
+  const onSubmit = data => console.log(data)
+
   return (
     <div>
       <img src={logo} alt='Logo React' />
-      <form onSubmit={() => {}}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor='firstName'>First Name</label>
         <input
           type='text'
           name='firstName'
           placeholder='Tu nombre'
+          {...register('firstName', { required: true, maxLength: 20 })}
         />
 
         <label htmlFor='lastName'>Last Name</label>
@@ -17,10 +23,11 @@ const ReactHookForm = () => {
           type='text'
           name='lastName'
           placeholder='Tu apellido'
+          {...register('lastName', { pattern: /^[A-Za-z]+$/i })}
         />
 
         <label htmlFor='gender'>Gender</label>
-        <select name='gender'>
+        <select name='gender' {...register('gender')}>
           <option value=''>Selecciona tu género</option>
           <option value='M'>Masculino</option>
           <option value='F'>Femenino</option>
@@ -32,12 +39,14 @@ const ReactHookForm = () => {
           type='email'
           name='email'
           placeholder='correo@mail.com'
+          {...register('email')}
         />
 
         <label htmlFor='password'>Password</label>
         <input
           type='password'
           name='password'
+          {...register('password')}
         />
 
         <button type='submit'>Register</button>
